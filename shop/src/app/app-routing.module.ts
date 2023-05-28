4baf015684e0c02502eaca94ca464dbac4226e15
hofy2 <61843032+hofy2@users.noreply.github.com>
2023-05-28 19:29:07 +0100

    Add files via upload

---
+++ shop/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ProductFormComponent } from './components/product-form/product-form.component';
+
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"login",component:LoginComponent},
+  {path:"products",component:ProductsComponent},
+  {path:"products/:id",component:ProductDetailsComponent},
+  {path:"products/:id/edit",component:ProductFormComponent},
+  {path:"**",component:NotFoundComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ shop/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductFormComponent } from './components/product-form/product-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ProductsComponent,
+    ProductDetailsComponent,
+    NotFoundComponent,
+    NavbarComponent,
+    ProductFormComponent
+  ],
+  imports: [
+    
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
